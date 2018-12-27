61f028d125799c2cad54007e1791861d463804e2
Timur Umerov <umerov.t32@gmail.com>
2018-12-27 21:22:32 +0300

    Init commit

---
+++ TestApplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Squirrel;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task.Run(
+            async () =>
+            {
+                using (var mgr = await UpdateManager.GitHubUpdateManager("https://github.com/UmerovT/TestApplication"))
+                {
+                    await mgr.UpdateApp();
+                }
+            });
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
